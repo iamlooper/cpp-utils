af35d31d343fed4dcaea9e96ae8fec2a3d42ee2f
iamlooper <iamloooper@gmail.com>
2023-06-17 02:10:06 +0500

    feat: add new functions and fix `get_paths_from_wp()`

--- util_functions.hpp
+++ util_functions.hpp
@@ -27,13 +27,19 @@ string exec_shell(const string& cmd, bool need_output);
 template <typename T, typename U>U convert(T value);
 pid_t get_pid(const string& process_name);
 vector<pid_t> get_pids(const string &process_name);
+pid_t get_proc_tid(const string &process_name, const string &thread_name);
+vector<pid_t> get_proc_tids(const string &process_name, const string &thread_name);
 void renice_process(const string& process_name, int new_priority);
 void kill_process(const string& process_name);
 void kill_processes(const string& process_name);
+void kill_thread(const string& process_name, const string& thread_name);
+void kill_threads(const string& process_name, const string& thread_name);
 void change_process_cpu_affinity(const string& process_name, int start, int end);
+void change_thread_cpu_affinity(const string& process_name, const string& thread_name, int start, int end);
 void change_process_scheduler(const string& process_name, const string& sched_policy, int priority);
-string get_home_pkgname();
-string get_ime_pkgname();
+void change_thread_scheduler(const string& process_name, const string& thread_name, const int& sched_policy, int priority);
+string get_home_pkg_name();
+string get_ime_pkg_name();
 void mlock_item(string mlock_type, string object);
 string escape_special_characters(const string& text);
 string grep_prop(const string& property, const string& filename);
@@ -173,7 +179,7 @@ pid_t get_pid(const string &process_name) {
   string output;
 
   // Build command to execute.
-  string cmd = "ps -Ao pid,args | grep -i -E '" + process_name + "' | awk '{print $1}' | head -n 1";
+  string cmd = "pidof -s " + process_name;
   
   // Use exec_shell() function to execute the shell command.
   output = exec_shell(cmd, true);  
@@ -196,10 +202,44 @@ vector<pid_t> get_pids(const string &process_name) {
   vector<pid_t> pid_list;
   stringstream ss(output);
   string pid;
-  while (getline(ss, pid, '\n')) { 
+  while (getline(ss, pid)) { 
     pid_list.push_back(convert<string, pid_t>(pid)); 
   }
   return pid_list;
+}
+
+pid_t get_proc_tid(const string &process_name, const string &thread_name) {
+  // Initialize variable to read output.
+  string output;
+
+  // Build command to execute.
+  string cmd = "pid=$(pidof -s " + process_name + "); for tid in $(ls /proc/$pid/task/); do thread_name=$(cat /proc/$pid/task/$tid/comm); [[ \"$(echo $thread_name | grep -i -E " + thread_name + ")\" != \"\" ]] && echo $tid && break; done";
+  
+  // Use exec_shell() function to execute the shell command.
+  output = exec_shell(cmd, true);  
+  
+  // Use convert() function to convert string to pid_t & return.
+  return convert<string, pid_t>(output);
+}
+
+vector<pid_t> get_proc_tids(const string &process_name, const string &thread_name) {
+  // Initialize variable to read output.
+  string output;
+
+  // Build command to execute.
+  string cmd = "pid=$(pidof -s " + process_name + "); for tid in $(ls /proc/$pid/task/); do thread_name=$(cat /proc/$pid/task/$tid/comm); [[ \"$(echo $thread_name | grep -i -E " + thread_name + ")\" != \"\" ]] && list=\"$list\n$tid\" && break; done";
+  
+  // Use exec_shell() function to execute the shell command.
+  output = exec_shell(cmd, true);    
+  
+  // Split the output by line, convert string to pid_t and store it in a vector.
+  vector<pid_t> list;
+  stringstream ss(output);
+  string line;
+  while (getline(ss, list)) { 
+    list.push_back(convert<string, pid_t>(output)); 
+  }
+  return list;    
 }
 
 void renice_process(const string& process_name, int new_priority) {
@@ -208,6 +248,14 @@ void renice_process(const string& process_name, int new_priority) {
 
   // Set the priority of the process.
   setpriority(PRIO_PROCESS, pid, new_priority);
+}
+
+void renice_thread(const string& process_name, const string& thread_name, int new_priority) {
+  // Get the TID of the given thread name using get_proc_tid() function.
+  pid_t tid = get_proc_tid(process_name, thread_name);
+
+  // Set the priority of the thread.
+  setpriority(PRIO_PROCESS, tid, new_priority);
 }
 
 // Process killer function.
@@ -228,6 +276,27 @@ void kill_processes(const string& process_name) {
   for (pid_t pid : pids) {
     // Send the SIGKILL signal to the process using the given PID.
     kill(pid, SIGKILL);  
+  }
+}
+
+// Thread killer function.
+void kill_thread(const string& process_name, const string& thread_name) {
+  // Get the TID of the given thread name using get_proc_tid() function.
+  pid_t tid = get_proc_tid(process_name, thread_name);
+    
+  // Send the SIGKILL signal to the process with the specified TID.
+  kill(tid, SIGKILL);
+}
+
+// Kill threads with same name.
+void kill_threads(const string& process_name, const string& thread_name) {
+  // Get list of tids & store as vector.
+  vector<pid_t> tids = get_proc_tids(process_name, thread_name);
+
+  // Iterate through the list of tids.
+  for (pid_t tid : tids) {
+    // Send the SIGKILL signal to the process using the given TID.
+    kill(tid, SIGKILL);  
   }
 }
 
@@ -254,6 +323,29 @@ void change_process_cpu_affinity(const string& process_name, int start, int end)
   }
 }
 
+// Change CPU affinity of a thread.
+void change_thread_cpu_affinity(const string& process_name, const string& thread_name, int start, int end) {
+  // Get the TID of the given thread name using get_proc_tid() function.
+  pid_t tid = get_proc_tid(process_name, thread_name);
+
+  // Initialize the mask to all zeros.
+  cpu_set_t mask;
+  CPU_ZERO(&mask);  
+
+  // Set the specified range of CPUs in the mask.
+  for (int i = start; i <= end; i++) {
+    CPU_SET(i, &mask);
+  }
+  
+  // Set the CPU affinity to the thread from the mask.
+  int result = sched_setaffinity(tid, sizeof(mask), &mask);
+  
+  // Check the return value to see if the call was successful.  
+  if (result == -1) {
+    xlog("error", "Failure setting CPU affinity: " + process_name + ":" + thread_name);
+  }
+}
+
 // Change the CPU scheduling policy of a process.
 void change_process_scheduler(const string& process_name, const int& sched_policy, int priority) {
   // Get the PID of the given process name using get_pid() function.
@@ -270,8 +362,24 @@ void change_process_scheduler(const string& process_name, const int& sched_polic
   }  
 }
 
+// Change the CPU scheduling policy of a thread.
+void change_thread_scheduler(const string& process_name, const string& thread_name, const int& sched_policy, int priority) {
+  // Get the TID of the given thread name using get_proc_tid() function.
+  pid_t tid = get_proc_tid(process_name, thread_name);
+
+  // Set the scheduling policy for the thread.
+  sched_param param;
+  param.sched_priority = priority;
+  int result = sched_setscheduler(tid, sched_policy, &param);
+
+  // Check the return value to see if the call was successful.
+  if (result == -1) {
+    xlog("error", "Failure changing scheduler: " + process_name + ":" + thread_name);
+  }  
+}
+
 // Get home launcher's package name.
-string get_home_pkgname() {
+string get_home_pkg_name() {
   // Initialize variable to read output.
   string output;
 
@@ -286,7 +394,7 @@ string get_home_pkgname() {
 }
 
 // Get IME's package name.
-string get_ime_pkgname() {
+string get_ime_pkg_name() {
   // Initialize variable to read output. 
   string output;
 
@@ -310,8 +418,6 @@ void preload_item(string item_type, string item) {
     cmd = "/data/local/tmp/vmtouch -dl " + item;
   } else if (item_type == "app") {
     cmd = "/data/local/tmp/vmtouch -dl \"$(pm path " + item + " | cut -d: -f2 | xargs dirname)\"";
-  } else if (item_type == "dex") {
-    cmd = "apk_path=\"$(pm path " + item + " | cut -d: -f2 | xargs dirname)\" && /data/local/tmp/vmtouch -dl \"$apk_path/oat\"";
   } else {
     return;
   }
@@ -408,34 +514,17 @@ bool is_path_exists(const string& path) {
 
 // Get list of paths from wildcard path. (e.g /foo/*.txt)
 vector<string> get_paths_from_wp(const string& wildcard_path) {
-  // Replace wildcard characters with their regex equivalents.
-  string regex_str = regex_replace(wildcard_path, regex(R"(\.)"), R"(\.)");
-  regex_str = regex_replace(regex_str, regex(R"(\*)"), R"(.*)");
-  regex_str = regex_replace(regex_str, regex(R"(\?)"), R"(.)");
-  regex path_regex(regex_str);
+  // Execute a shell command to retrieve the output of the wildcard expansion.
+  string output = exec_shell("for i in " + wildcard_path + "; do echo \"$i\"; done", true);
 
-  // Initialize a vector to store matched paths.
-  vector<string> matched_paths;
-
-  // Get the base path from the wildcard path.
-  filesystem::path base_path = filesystem::path(wildcard_path).parent_path();
-  
-  // Check if the base path exists.
-  if (!is_path_exists(base_path)) {
-    xlog("error", "Base path does not exist: " + (string)base_path);
-    return matched_paths;
-  }  
-
-  // Iterate through the directory entries.
-  for (const auto& entry : filesystem::directory_iterator(base_path)) {
-    // Check if the current entry's path matches the regex pattern.
-    if (regex_match(entry.path().string(), path_regex)) {
-      matched_paths.push_back(entry.path().string());
-    }
+  // Split the output by line and store it in a vector.
+  vector<string> list;
+  stringstream ss(output);
+  string line;
+  while (getline(ss, list)) { 
+    list.push_back(list); 
   }
-
-  // Return list of matched paths from wildcard path.
-  return matched_paths;
+  return list;  
 }
 
 // remove() with support to delete wildcard path.
@@ -443,12 +532,7 @@ void remove_path(const string& path) {
   // If the path contains an asterisk, then use the get_paths_from_wp() function to get a list of paths.
   if (path.find('*') != string::npos) { 
     vector<string> paths;
-    // Catch an exception.
-    try {
-      paths = get_paths_from_wp(path);
-    } catch (const exception& e) {
-      xlog("error", "An exception occurred: " + (string)e.what());
-    }    
+    paths = get_paths_from_wp(path);
     for (const std::string& path : paths) {
       remove(path.c_str());
     }
